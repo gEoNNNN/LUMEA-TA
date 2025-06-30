b97f5704e8d7577005b5cecf964558be2f232a7a
Eugen Ostafi <eugen.ostafi@isa.utm.md>
2025-06-30 23:29:28 +0300

    blog page done

--- src/pages/BlogPage.tsx
+++ src/pages/BlogPage.tsx
@@ -4,14 +4,17 @@ import NavBar from "../components/navbar";
 import translations from "./BlogPage.json";
 import Footer from "../components/footer";
 import Bg from "../assets/blogmainbg.jpg";
-import Contacts from "../components/contacts"
-import secBg from "../assets/blogsecondbg.png"
-import blueline from "../assets/blueline.svg"
-import blogsection1 from "../assets/blogsection1.jpg"
-import biglueline from "../assets/longblueline.svg"
-import card1 from "../assets/blogcard1.jpg"
-import card2 from "../assets/blogcard2.jpg"
-import card3 from "../assets/blogcard3.jpg"
+import Contacts from "../components/contacts";
+import secBg from "../assets/blogsecondbg.png";
+import blueline from "../assets/blueline.svg";
+import blogsection1 from "../assets/blogsection1.jpg";
+import biglueline from "../assets/longblueline.svg";
+import card1 from "../assets/blogcard1.jpg";
+import card2 from "../assets/blogcard2.jpg";
+import card3 from "../assets/blogcard3.jpg";
+import article4image from "../assets/article4image.jpg";
+import cardblog1 from "../assets/cardblog1.jpg";
+import cardblog2 from "../assets/cardblog2.jpg";
 
 const BlogPage: React.FC = () => {
   const [currentLang, setCurrentLang] = useState<'ro' | 'ru'>('ro');
@@ -48,9 +51,33 @@ const BlogPage: React.FC = () => {
                 <li className='blogpage-articles-2-list-item'><img src={card3}/></li>
             </ul>
         </div>
-            <div className='blogpage-articles-3'>
-                <p className='blogpage-articles-3-title'>{translations[currentLang].blog.section3title}</p>
-                <p className='blogpage-articles-3-descritpion' dangerouslySetInnerHTML={{ __html: translations[currentLang].blog.section3description }}/>
+        <div className='blogpage-articles-3'>
+            <p className='blogpage-articles-3-title'>{translations[currentLang].blog.section3title}</p>
+            <p className='blogpage-articles-3-descritpion' dangerouslySetInnerHTML={{ __html: translations[currentLang].blog.section3description }}/>
+        </div>
+        <div className='blogpage-articles-4'>
+            <img src={blueline} className='blogpage-articles-4-blueline'/>
+            <p className='blogpage-articles-4-title'>{translations[currentLang].blog.section4title}</p>
+            <p className='blogpage-articles-4-descritpion-1'>{translations[currentLang].blog.section4descritpion1}</p>
+            <img src={article4image} className='blogpage-articles-4-image'/>
+            <p className='blogpage-articles-4-descritpion-2'>{translations[currentLang].blog.section4descritpion2}</p>
+            <img src={biglueline} className='blogpage-articles-1-longblueline'/>
+        </div>
+        <div className='blogpage-articles-5'>
+            <p className='blogpage-articles-5-title'>{translations[currentLang].blog.section5title}</p>
+            <p className='blogpage-articles-5-descritpion' dangerouslySetInnerHTML={{ __html: translations[currentLang].blog.section5description1 }} />
+            <p className='blogpage-articles-5-descritpion'>{translations[currentLang].blog.section5description2}</p>
+            <p className='blogpage-articles-5-descritpion-bold'>{translations[currentLang].blog.section5description3}</p>
+            <p className='blogpage-articles-5-descritpion' dangerouslySetInnerHTML={{ __html: translations[currentLang].blog.section5description4 }} />
+            <img src={cardblog1} className='blogpage-articles-5-card' />
+            <img src={cardblog2} className='blogpage-articles-5-card' />
+        </div>
+        <div className='blogpage-articles-6'>
+            <p className='blogpage-articles-6-title'>{translations[currentLang].blog.section6title}</p>
+            <p className='blogpage-articles-6-descritpion-1'>{translations[currentLang].blog.section6description1}</p>
+            <p className='blogpage-articles-6-descritpion-2' dangerouslySetInnerHTML={{ __html: translations[currentLang].blog.section6description2 }} />
+            <p className='blogpage-articles-6-descritpion-3' dangerouslySetInnerHTML={{ __html: translations[currentLang].blog.section6description3 }} />
+            <p className='blogpage-articles-6-descritpion-4' dangerouslySetInnerHTML={{ __html: translations[currentLang].blog.section6description4 }} />
         </div>
       </div>
       <Contacts currentLang={currentLang}/>
